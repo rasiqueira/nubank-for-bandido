1328c459ca05b2a69f05a002dc915fddeccc318e
mateusmoov <mateusvasconcelos4210@gmail.com>
2024-07-23 19:15:54 -0300

    refactor: create list buttons

--- app/index.tsx
+++ app/index.tsx
@@ -1,20 +1,6 @@
-import ActionButton from "@/components/ActionButton";
 import Header from "@/components/Header";
-import { View, Text, StyleSheet, Animated, FlatList } from "react-native";
-import PixIcon from "@/assets/images/icons/pix.svg";
-import BoletoIcon from "@/assets/images/icons/boleto.svg";
-import TranfearIcon from "@/assets/images/icons/transfer.svg";
-import DepositIcon from "@/assets/images/icons/deposit.svg";
-import PhoneIcon from "@/assets/images/icons/phone.svg";
-
-const iconItems = [
-  { id: 1, title: "Área Pix", image: PixIcon },
-  { id: 2, title: "Boleto", image: BoletoIcon },
-  { id: 3, title: "Transferência", image: TranfearIcon },
-  { id: 4, title: "Depósito", image: DepositIcon },
-  { id: 5, title: "Recarga", image: PhoneIcon },
-];
-const scrollY = new Animated.Value(0);
+import { View, Text, StyleSheet } from "react-native";
+import ListButtons from "@/components/ListButtons";
 
 export default function Index() {
   return (
@@ -35,23 +21,7 @@ export default function Index() {
             <Text style={styles.moneyText}>R$ 3.231,01</Text>
           </View>
         </View>
-        <Animated.FlatList
-          data={iconItems}
-          style={{ marginTop: 25 }}
-          renderItem={({ item }) => (
-            <View style={{ marginRight: 10 }}>
-              <ActionButton IconComponent={item.image} title={item.title} />
-            </View>
-          )}
-          keyExtractor={(item) => item.id.toString()}
-          onScroll={Animated.event(
-            [{ nativeEvent: { contentOffset: { y: scrollY } } }],
-            { useNativeDriver: false }
-          )}
-          scrollEventThrottle={16}
-          horizontal
-          showsHorizontalScrollIndicator={false}
-        />
+        <ListButtons />
       </View>
     </>
   );
